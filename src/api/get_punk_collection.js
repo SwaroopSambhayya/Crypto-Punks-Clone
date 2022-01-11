f748ab3e55d8fc6e9458833995bec41713758679
Swaroop Sambhayya <swaroop.sambhayya@gmail.com>
2022-01-11 17:19:13 +0530

    bug fixes

--- src/api/get_punk_collection.js
+++ src/api/get_punk_collection.js
@@ -2,9 +2,13 @@ import axios from "axios";
 import { dummyList } from "../dummyData";
 
 export const getPunkList = async (setPunkList) => {
-  var response = await axios.get(
-    "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x238f65eF9231d86bd9Bb2eFDeAF4306fd69e2A89"
-  );
+  try {
+    var response = await axios.get(
+      "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x238f65eF9231d86bd9Bb2eFDeAF4306fd69e2A89"
+    );
+  } catch (e) {
+    console.log(e);
+  }
 
   setPunkList(
     response?.data?.assets !== undefined ? response.data.assets : dummyList
