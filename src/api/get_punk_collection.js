42136f60d7abe2aec3cb1418d4e2a11fa17cf6cb
Swaroop Sambhayya <swaroop.sambhayya@gmail.com>
2022-01-11 17:12:56 +0530

    Network exceptions

--- src/api/get_punk_collection.js
+++ src/api/get_punk_collection.js
@@ -6,6 +6,8 @@ export const getPunkList = async (setPunkList) => {
     "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x238f65eF9231d86bd9Bb2eFDeAF4306fd69e2A89"
   );
 
-  setPunkList(response.status === 200 ? response.data.assets : dummyList);
+  setPunkList(
+    response?.data?.assets !== undefined ? response.data.assets : dummyList
+  );
   return response.data.assets;
 };
