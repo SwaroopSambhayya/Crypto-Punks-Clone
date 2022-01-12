9e1009da51a25482dbc61c74099301d5a34010c3
Swaroop Sambhayya <swaroop.sambhayya@gmail.com>
2022-01-12 17:46:59 +0530

    mobile responsive

--- src/api/get_punk_collection.js
+++ src/api/get_punk_collection.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { dummyList } from "../dummyData";
 
 export const getPunkList = async (setPunkList) => {
-  try {
+  /*enable this in dev mode
+    try {
+      
     var response = await axios.get(
       "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x238f65eF9231d86bd9Bb2eFDeAF4306fd69e2A89"
     );
@@ -13,6 +15,8 @@ export const getPunkList = async (setPunkList) => {
   setPunkList(
     response?.data?.assets !== undefined ? response.data.assets : dummyList
   );
-  console.log("Cool", response.data.assets);
-  return response.data.assets;
+  */
+  setPunkList(dummyList);
+
+  return dummyList;
 };
